2d5d2d52219cc41854afd3222b8cf63bfdd24411
cheng93 <derek.c@hotmail.co.uk>
2018-09-20 19:44:03 +0100

    TripleTriad.Logic

---
+++ src/TripleTriad.Logic/Entities/GameData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TripleTriad.Logic.Entities
+{
+    public class GameData
+    {
+        public GameStatus Status { get; set; } = GameStatus.Waiting;
+
+        public ICollection<string> Log { get; set; } = new List<string>();
+    }
+}
---
+++ src/TripleTriad.Logic/Entities/GameStatus.cs
@@ -0,0 +1,12 @@
+namespace TripleTriad.Logic.Entities
+{
+    public enum GameStatus
+    {
+        Waiting = 0,
+        InProgress = 1,
+        Forfeit = 2,
+        PlayerOneWin = 3,
+        PlayerTwoWin = 4,
+        Tie = 5
+    }
+}
