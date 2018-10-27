354abaa29053c61500b4ba33c7311ebfaa8aaed4
cheng93 <derek.c@hotmail.co.uk>
2018-10-27 19:55:24 +0100

    when page changes emit change page action

--- src/TripleTriad.Client/src/app/games/containers/select-cards/select-cards.component.ts
+++ src/TripleTriad.Client/src/app/games/containers/select-cards/select-cards.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromStore from '../../reducers';
 import { filter, tap } from 'rxjs/operators';
-import { LoadAllCards } from '../../actions/select-cards.actions';
+import { LoadAllCards, ChangePage } from '../../actions/select-cards.actions';
 import { Observable, Subscription } from 'rxjs';
 import { Card } from '../../models/card';
 
@@ -13,13 +13,17 @@ import { Card } from '../../models/card';
 })
 export class SelectCardsComponent implements OnInit, OnDestroy {
   constructor(private store: Store<fromStore.GamesState>) {
-    this.cards$ = store.select(fromStore.getAllCards);
+    this.cards$ = store.select(fromStore.getLevelCards);
     this.selectedCards$ = store.select(fromStore.getSelectedCards);
   }
 
   cards$: Observable<Card[]>;
 
   selectedCards$: Observable<Card[]>;
+
+  changePage($event: number) {
+    this.store.dispatch(new ChangePage($event));
+  }
 
   ngOnInit() {
     this.subscription = this.store
