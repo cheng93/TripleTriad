017ed1cacea7cea096a3083be20fa59c314f9ff5
cheng93 <derek.c@hotmail.co.uk>
2018-10-27 20:51:49 +0100

    selected-card-list component

---
+++ src/TripleTriad.Client/src/app/games/components/selected-card-list/selected-card-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Card } from '../../models/card';
+
+@Component({
+  selector: 'app-selected-card-list',
+  templateUrl: './selected-card-list.component.html',
+  styleUrls: ['./selected-card-list.component.scss']
+})
+export class SelectedCardListComponent {
+  constructor() {}
+
+  @Input()
+  cards: Card[];
+
+  @Output()
+  removeCard: EventEmitter<Card> = new EventEmitter();
+
+  remove(card: Card) {
+    this.removeCard.emit(card);
+  }
+
+  columns: string[] = [
+    'name',
+    'top',
+    'right',
+    'bottom',
+    'left',
+    'element',
+    'actions'
+  ];
+}
