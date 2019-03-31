d4e785886d34ac8f70cb8ff7d1ff6788c332b1e2
cheng93 <derek.c@hotmail.co.uk>
2019-03-31 21:50:12 +0100

    Add JoinLobby to GameHub

--- src/TripleTriad.SignalR/GameHub.cs
+++ src/TripleTriad.SignalR/GameHub.cs
@@ -8,6 +8,12 @@ namespace TripleTriad.SignalR
     [Authorize(Policy = AuthConstants.TripleTriadScheme)]
     public class GameHub : Hub<IGameClient>
     {
+        public const string Lobby = "lobby";
+
+        public async Task JoinLobby()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, Lobby);
+        }
         public async Task ViewGame(int gameId)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, gameId.ToString());
