9d785f41861a81f52242a68c2de02fb8249c2daa
cheng93 <derek.c@hotmail.co.uk>
2019-04-20 23:52:03 +0100

    connect to signalR on app load

--- src/TripleTriad.Client/src/app/app.component.ts
+++ src/TripleTriad.Client/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SignalRFacade } from './core/services/signal-r.facade';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private signalRFacade: SignalRFacade) {}
+
+  ngOnInit(): void {
+    this.signalRFacade.connect();
+  }
+
   title = 'app';
 }
