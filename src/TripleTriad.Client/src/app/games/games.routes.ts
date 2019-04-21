5a7ca8649d1fb1615577b97b6af9115fbb0c243f
cheng93 <derek.c@hotmail.co.uk>
2019-04-21 16:58:59 +0100

    sort out routes

--- src/TripleTriad.Client/src/app/games/games.routes.ts
+++ src/TripleTriad.Client/src/app/games/games.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
-import { GameLobbyComponent } from './containers/game-lobby/game-lobby.component';
 import { GameRoomComponent } from './containers/game-room/game-room.component';
 
 export const routes: Routes = [
-  { path: '', component: GameLobbyComponent },
   { path: ':gameId', component: GameRoomComponent }
 ];
