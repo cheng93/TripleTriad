d19b041ca245128571946b461726011c921e13b8
cheng93 <derek.c@hotmail.co.uk>
2019-04-21 15:17:21 +0100

    game-list component

--- src/TripleTriad.Client/src/app/lobby/lobby.module.ts
+++ src/TripleTriad.Client/src/app/lobby/lobby.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 
+import { GameListComponent } from './components/game-list/game-list.component';
 import { LobbyComponent } from './containers/lobby/lobby.component';
 import { LobbyEffects } from './effects/lobby.effects';
 import { LobbyService } from './services/lobby.service';
@@ -11,12 +13,13 @@ import * as fromLobby from './reducers/lobby.reducer';
 import { routes } from './lobby.routes';
 
 @NgModule({
-  declarations: [LobbyComponent],
+  declarations: [LobbyComponent, GameListComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     StoreModule.forFeature('lobby', fromLobby.reducer),
-    EffectsModule.forFeature([LobbyEffects])
+    EffectsModule.forFeature([LobbyEffects]),
+    MatTableModule
   ],
   providers: [LobbyService]
 })
